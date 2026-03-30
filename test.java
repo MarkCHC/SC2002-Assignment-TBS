66dcf76bc5aadf16162e348404c42273a4930a19
manasraghu <rmanas971@gmail.com>
2026-03-30 13:24:37 +0800

    initial commit

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Simple math test
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum: " + (a + b));
+        
+        // Loop test
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
