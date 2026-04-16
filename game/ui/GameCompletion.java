60bb661f6fff6058325a963c0a9d38518bb031ba
CoolHazardous <sharkcraft89@gmail.com>
2026-04-16 16:01:48 +0800

    Update GameCompletion.java
    Created game completion screen

--- game/ui/GameCompletion.java
+++ game/ui/GameCompletion.java
@@ -1,5 +1,22 @@
 package game.ui;
+
 public class GameCompletion {
-    public static void playerWin() {}
-    public static void playerLose() {}
+
+    public static void playerWin() {
+        System.out.println("\n==================================================================");
+        System.out.println("||                                                              ||");
+        System.out.println("||                    V I C T O R Y !                           ||");
+        System.out.println("||             All enemy waves have been cleared.               ||");
+        System.out.println("||                                                              ||");
+        System.out.println("==================================================================\n");
+    }
+
+    public static void playerLose() {
+        System.out.println("\n==================================================================");
+        System.out.println("||                                                              ||");
+        System.out.println("||                     D E F E A T .                            ||");
+        System.out.println("||              Your Character has been defeated.                  ||");
+        System.out.println("||                                                              ||");
+        System.out.println("==================================================================\n");
+    }
 }
